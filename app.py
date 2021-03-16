1cc702864d3a82f12f6932755a29b6e03c494ee7
eshankt <eshankt@outlook.com>
2021-03-16 16:32:18 +0530

    changes by munwar by

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
-@app.route("/")
+#@app.route("/")
 def home():
     #return "Hello, Flask!"
     #<img src="https://ih1.redbubble.net/image.1220724104.4638/farp,small,wall_texture,product,750x1000.u10.jpg" align="middle" />
