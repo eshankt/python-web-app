eec2d52dc88ffa1c806d3b7443b47462b4f2bd22
eshankt <eshankt@outlook.com>
2021-03-16 17:40:21 +0530

    align width

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import render_template
 app = Flask(__name__)
 @app.route("/")
 def home():     
-    return"<h1><center>Hello, From ME! <img src='https://media.heartlandtv.com/images/Pool1.PNG' width='500' height='600'></h1></center>"
+    return"<h1><center>Hello, From ME! <img src='https://media.heartlandtv.com/images/Pool1.PNG' width='100' height='200'></h1></center>"
 
     
